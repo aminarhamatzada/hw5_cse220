8aa34182113b9a0cdb2264c2445fb37b76f91fee
aminarhamatzada <121897630+aminarhamatzada@users.noreply.github.com>
2024-05-03 21:02:31 +0000

    17

--- hw5.asm
+++ hw5.asm
@@ -60,6 +60,7 @@ print_student:
 init_student_array:
 	
 	# preamble the values #
+
 	addi $sp, $sp, -32
 	sw $ra, 0($sp)
 
@@ -81,15 +82,13 @@ init_student_array:
 	move $s3, $a3		# move names pointer
 	
 	# we reached the limit #
+
 	lw $s4, 32($sp)		# move student records
 	li $s5, 0			# set counter to 0
 	
 	loop:
 
 		beq $s5, $s0, done	# terminate loop when if counter >= num_student
-		
-		sll $s6, $s5, 3   	# multiply loop counter by 8
-        add $t7, $t4, $t6	# calculate address 
         	
 		lw $t1, 0($s1)      # load id
 		move $a0, $t1		# move id
@@ -102,7 +101,7 @@ init_student_array:
         	
 		jal init_student	# call init function
 		
-		# create loop to go through name
+		# create loop to go through name #
 		
 		inner_loop:
 		
@@ -112,10 +111,12 @@ init_student_array:
 			# compare to 0, if zero branch to inner done
 			# if not move char array over one (addi), then j inner_loop
 
-			beqz $t0, done		# exit if name is '\0'
+			beqz $t1, inner_done		# exit if name is '\0'
 
 			addi $s3, $s3, 1	# increment names pointer
-			addi $s5, $s5, 1 	# increment loop counter
+
+			#sll $s6, $s5, 3  	# multiply loop counter by 8
+        	#add $t7, $t4, $t6	# calculate address 
 			
 			j inner_loop
 		
@@ -125,8 +126,8 @@ init_student_array:
 			addi $s2, $s2, 4	# increment credits
 			addi $s3, $s3, 1	# increment name pointer
 			addi $s4, $s4, 8	# increment record
-			addi $t5, $t5, 1 	# increment loop counter
-		
+			addi $s5, $s5, 1 	# increment loop counter
+			
 			j loop 			# jump back to the begninning
 		
 	done:
@@ -147,9 +148,48 @@ init_student_array:
 	jr $ra	
 
 insert:
+
+	# a0 - pointer to record, a1 - pointer to hash table, a2 - table size
+
+	lw $t0, 0($a0) 		# load record pointer
+	srl $t1, $t0, 10 	# shift t0 to access id 
+
+	div $t1, $a2 	# then divide id by table size 
+	mfhi $t1 		# mfhi remainder, index that i want to insert
+
+	# loop for linear probing #
+
+	li $t4, 0 		# loop counter
+
+	loop_linear_probing :
+
+		sll $t2, $t1, 3 	# calculate offset for hash index
+		add $t3, $a1, $t2 	# calculate address in table
+
+		lw $t5, 0($t3)		# load value from hash table
+
+		addi $t1, $t1, 1	# increment hash index
+		addi $t4, $t4, 1	# increment counter
+
+
+    	bne $t4, $a2, loop_linear_probing 
+
+		# check if table is full, return -1 #
+		li $v0, -1			# returns -1
+
+	record: 
+
+		sw $a0, 0($t3)      # insert record into hash table
+    	move $v0, $t1       # return index
+
 	jr $ra
 	
 search:
+
+	# a0 - id, a1 - pointer to hash, a2 - table size
+
+	# use branch to return something if we fail $v0 #
+
 	jr $ra
 
 delete:
